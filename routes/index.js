8460eb50e8d68816754dad8144f19b0ec6337350
gokufa12 <bdoyle@turnkey-technology.com>
2016-01-26 22:52:02 -0500

    Initial CRUD Stuff
    Adding initial CRUD stuff.
    
    DB config will be overwritten by the script db_init:replace
    Has various SQL statements that may or may not be needed.
    Implemented Create for arranger.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,58 @@
 var express = require('express');
 var router = express.Router();
+var pgp = require('pg-promise')();
+/* Config details for the database */
+var config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'postgres',	//Plan to change this in the future
+    user: 'db_user',
+};
+var db = pgp(config);
+
+/* SQL */
+var arranger_insert = "INSERT INTO arranger (name, \"e-mail\") VALUES ($1,$2)";
+var arranger_select = "SELECT arranger_id FROM arranger WHERE name = $1";
+var song_select = "SELECT title, date, avg_rating, avg_difficulty FROM song";
+var song_title_arranger_select = "SELECT song_id FROM song NATURAL JOIN arranger WHERE title = $1 AND name = $2";
+var song_insert = "INSERT INTO song (title, arranger_id, date) VALUES ($1,$2,$3)";
+var reviewer_select = "SELECT name FROM reviewer";
+var instrument_view = "SELECT instrument_id, name as instrument FROM instruments "
+            + "NATURAL JOIN $1 ON instrument_id";
+var review_select = "SELECT instrument.name, part_rating, part_difficulty "
+            + "FROM review NATURAL JOIN reviewer NATURAL JOIN instrument "
+            + "WHERE song_id = $1";
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* Arranger CRUD */
+
+//Arranger Create
+router.post('/api/v1/arranger', function(req, res) {
+	//This may be able to be done by including :name and :email in the url, but will need to investigate
+    var vals = [req.body.name, req.body.email];
+    db.none(arranger_insert, vals)
+    .then(function() {
+        db.query("SELECT * FROM arranger ORDER BY name ASC")
+        .then(function(data) {
+            console.log(data);
+            res.status(200).json(data);
+        })
+    })
+    .catch(function(err) {
+        console.log(err);
+        return res.status(500).json({success: false, reason: err});
+    });
+});
+
+/* Song CRUD */
+
+/* Reviewer CRUD */
+
+/* Review CRUD */
 
 module.exports = router;
