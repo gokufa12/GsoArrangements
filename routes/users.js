7a15c671d04963fc98b3c51ef56b5b1d17be46ef
gokufa12 <bdoyle@turnkey-technology.com>
2016-09-14 21:36:09 -0400

    Template all the things
    Move things to use ejs for templates.
    Adds ejs templates.
    Removes old html files.

--- routes/users.js
+++ routes/users.js
@@ -4,19 +4,19 @@ var path = require('path');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../views/users', 'user_profile.html'));
+  res.render('../views/users/user_profile', { title: 'GSO Arrangements Dashboard'});
 });
 
 router.get('/addsong', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../views/users', 'song.html'));
+  res.render('../views/users/song', { title: 'GSO Arrangments - Add Song'});
 });
 
 router.get('/addsong_csv', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../views/users', 'upload_song_csv.html'));
+  res.render('../views/users/upload_song_csv', { title: 'GSO Arrangements - Bulk Song Add'});
 });
 
 router.get('/arrangements', function(req, res, next) {
-  res.sendFile(path.join(__dirname, '../views/users', 'arrangements.html'));
+  res.render('../views/users/arrangements', { title: 'GSO Arrangements - View Arrangements'});
 });
 
 module.exports = router;
