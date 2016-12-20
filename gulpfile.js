69c879083266aff587abffba98c7f74049a398a3
alex <ameasday@terpmail.umd.edu>
2016-12-20 02:58:47 +0000

    work has begun on task automation via gulp. next steps are to reorganize code into client/server dirs

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp        = require('gulp');
+var gutil       = require('gulp-util');
+var jshint      = require('gulp-jshint');
+var uglify      = require('gulp-uglify');
+var concat      = require('gulp-concat');
+var nodemon     = require('gulp-nodemon');
+var livereload  = require('gulp-livereload');
+
+
+// executes without any additional args; the default
+gulp.task('default', ['watch-changes']);
+
+gulp.task('watch-changes', function() {
+    gulp.watch('src/**/*.js', ['jshint', 'uglify-js']); 
+});
+
+// execute to start up the server
+gulp.task('run', ['start-server']);
+
+gulp.task('start-server', function() {
+    livereload.listen();
+    
+    nodemon({
+        script: './bin/www',
+        ext: 'html js css',
+        env: {
+            env: 'development'
+        }
+    }).on('restart', function() {
+        gulp.src('server.js')
+            .pipe(livereload());
+    });
+});
+
+// jslint - to force us to write better code
+gulp.task('jshint', function() {
+    gulp.src('src/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// minifies all JS files from the client/server code
+gulp.task('uglify-js', function() {
+    gulp.src('src/**/*.js')
+        .pipe(uglify())
+        .pipe(concat('minified.js'))
+        .pipe(gulp.dest('out'));
+});
